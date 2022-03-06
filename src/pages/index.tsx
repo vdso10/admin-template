05e815b96b15a9500a94a4266e7bfc4e787c20a2
vdso10 <53046799+vdso10@users.noreply.github.com>
2022-03-06 12:08:04 -0300

    Commit-1

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="left-6 bg-slate-600">Admin</h1>
+      
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
